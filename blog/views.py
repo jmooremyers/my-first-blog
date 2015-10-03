6262f2ee3ba46ee525d7c8b73b9af75eb2ef819f
Jennifer Moore Myers <jamyers@ncsu.edu>
2015-10-03 15:42:15 -0400

    added sample leaflet map and view

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
 
+def view_map(request):
+    return render (request, 'blog/map.html', {})
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
